c13ce5fc193938c6aa8bb6219a66c7a220d913c3
Asha Fernandez <ash.fernan90@gmail.com>
2019-12-16 00:59:46 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,12 @@
-def hello_t
+def hello(array)
+  i=0
+  while i < array.length
+  yield (array[i)])
+   i+=1
+ end
+end
+hello (["Tim","Tom", "Jim"]) {|name| puts "Hi, #{name}"}
 
 end
-
 # call your method here!
 
