5a41a6e16c3b87e5571cfb69899739a449ec44da
Mikael Kaya <kayaswork@gmail.com>
2019-12-22 02:19:12 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,12 @@
-def hello_t
-
+def map(array)
+    i = 0
+    while array[i] do
+    yield(array, i)
+    end
+map([1, 2, 3], i) do |x|
+while x[i] do
+neg_arr = []
+ y = array[i] * -1
+neg_arr << y
+i += 1
 end
-
-# call your method here!
-
